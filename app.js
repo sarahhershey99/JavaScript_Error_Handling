c5f08428f7082bcab9920022a47bfdaa8c300430
Sarah Hershenhouse <sarahhershey99@gmail.com>
2024-01-07 21:26:06 -0700

    Completed-Error-Handling-Exercise

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+  try {
+    return JSON.parse(data); 
+  } catch(err) {
+    console.error(err);
+    return null;
+  };
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
-}
+};
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
